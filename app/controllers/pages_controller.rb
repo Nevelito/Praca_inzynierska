54c1d81b9d9ffd7f46511e17a31c6355042be283
Nevelito <szymonnewel@gmail.com>
2024-07-08 11:03:16 +0200

    add rspec and rubocop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:home]
 
   def home
-    render "pages/home"
+    render 'pages/home'
   end
 end
