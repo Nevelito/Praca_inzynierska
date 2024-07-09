23fa47d04e48f74aee0ecfcdd57c9201a2b80579
Nevelito <szymonnewel@gmail.com>
2024-07-09 14:51:45 +0200

    add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def check_if_active(controllers)
+    "border-x-2 border-t-2 border-t-blue-700 border-x-blue-700 rounded-t-lg bg-neutral-500" if controllers.any?(params[:controller])
+  end
 end
